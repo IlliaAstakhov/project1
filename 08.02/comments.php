70a420ea7299410f651ce1107d8c1b35dbe47978
astahovillia <astahovillia@gmail.com>
2021-02-08 20:07:52 +0200

    comments php

--- 08.02/comments.php
+++ 08.02/comments.php
@@ -1,3 +1,11 @@
-<?php echo 'Test massage', PHP_EOL; ?>
+<?php
 
-<?= 'Test22 massage', PHP_EOL; ?>
+# echo 'test 01', PHP_EOL;
+//  cntrl /  - однострочный коммент
+//  cntrl + shipt+ / - многострочный коммент
+
+echo 'test 03', PHP_EOL;
+echo 'test 04', PHP_EOL;
+echo 'test 05', PHP_EOL;
+
+echo 'test 06', PHP_EOL;
