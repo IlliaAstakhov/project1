d24a1f0da96fbb77362ac947f7f46907b1e98f2e
astahovillia <astahovillia@gmail.com>
2021-02-18 22:00:57 +0200

    ngnix up

--- 1102/1102.php
+++ 1102/1102.php
@@ -1,2 +1,25 @@
 <?php
-echo 'Hello', PHP_EOL;
+//$programLanguegeNew = [
+//    'PHP',
+//    'JS',
+//    'C++',
+//
+//];
+//unset($programLanguegeNew[1]);
+//
+//var_dump ($programLanguegeNew);
+//print_r($programLanguegeNew);
+
+$student = [
+    'name' => 'Vasyl',
+    'age' => '22',
+    'isGoodBoy' => true,
+    'gender' => 'male',
+    'programminLanguges' => [
+        'PHP',
+        'JS',
+        'C++',
+    ],
+];
+//print_r($student);
+var_dump($student['programminLanguges'][1]);
---
+++ 1102/sort_array.php
@@ -0,0 +1,21 @@
+<?php
+$programLanguegeNew = [
+    'PHP',
+    'JS',
+    'C++',
+    'S#',
+    'Python',
+];
+
+$numbers = [
+    8,
+    1,
+    3,
+    88,
+    10,
+    98,
+];
+asort($numbers);
+//arsort($programLanguegeNew);
+
+print_r($numbers);
